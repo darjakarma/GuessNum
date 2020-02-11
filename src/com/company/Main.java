46dd5c04d931cf46f898a7c0a98af91e6f69b424
JavaCourses1 <atdk.fb@gmail.com>
2020-02-11 19:29:08 +0200

    valdation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,10 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+    static Scanner scan = new Scanner(System.in); // глобальный скан
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        Random rand = new Random();
+        Random rand = new Random(); // локальная переменная
 
         System.out.println("What is your name?");
         String name = scan.next();
@@ -17,12 +17,31 @@ public class Main {
         int myNum = rand.nextInt(100) + 1;
         System.out.println("Cheat: " + myNum);
 
-        for (int i = 0; i < 9; i++) {
-            System.out.println("Enter your guess");
-            int userNum = scan.nextInt();
-        }
+        for (int i = 0; i < 10; i++) {
+            int userNum = askGuess();// локальная переменная
 
-            if (myNum == userNum) {
+
+            if (myNum < userNum) {
+                System.out.println("My num is less then yours!");
+            } else if (myNum > userNum) {
+                System.out.println("My num is greater then yours!");
+            } else {
                 System.out.println("You WIN!");
-            } break;
+            }
+            break;
         }
+    }
+
+    static int askGuess() {
+
+        while (true) {
+            System.out.println("Enter your guess");
+            int num = scan.nextInt();
+            if (num >= 1 && num <= 100) { // если условие перед ретурном - не будет работать!
+                return num;
+            } else {
+                System.out.println("Please enter a number from 1 to 100");
+            }
+        }
+    }
+}
