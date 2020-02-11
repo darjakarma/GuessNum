23682894893436999cff0a98282ad295ef89c85a
JavaCourses1 <atdk.fb@gmail.com>
2020-02-11 20:57:10 +0200

    valdation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,8 @@
 package com.company;
 
 
+import java.nio.channels.ScatteringByteChannel;
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,39 +10,49 @@ public class Main {
     static Scanner scan = new Scanner(System.in); // глобальный скан
 
     public static void main(String[] args) {
+
         Random rand = new Random(); // локальная переменная
+        String answer;
 
-        System.out.println("What is your name?");
-        String name = scan.next();
-        System.out.println("Hello, " + name);
+        do {
+            System.out.println("What is your name?");
+            String name = scan.next();
+            System.out.println("Hello, " + name);
 
-        int myNum = rand.nextInt(100) + 1;
-        System.out.println("Cheat: " + myNum);
+            int myNum = rand.nextInt(100) + 1;
+            System.out.println("Cheat: " + myNum);
 
-        for (int i = 0; i < 10; i++) {
-            int userNum = askGuess();// локальная переменная
+            for (int i = 0; i < 10; i++) {
+                int userNum = askGuess();// локальная переменная
 
 
-            if (myNum < userNum) {
-                System.out.println("My num is less then yours!");
-            } else if (myNum > userNum) {
-                System.out.println("My num is greater then yours!");
-            } else {
-                System.out.println("You WIN!");
+                if (myNum < userNum) {
+                    System.out.println("My num is less then yours!");
+                } else if (myNum > userNum) {
+                    System.out.println("My num is greater then yours!");
+                } else {
+                    System.out.println("You WIN!");
+                    break;
+                }
             }
-            break;
-        }
+            System.out.println("DO you want play?");
+            answer = scan.next();
+        } while (answer.equals("yes"));
     }
 
     static int askGuess() {
-
-        while (true) {
-            System.out.println("Enter your guess");
-            int num = scan.nextInt();
-            if (num >= 1 && num <= 100) { // если условие перед ретурном - не будет работать!
-                return num;
-            } else {
-                System.out.println("Please enter a number from 1 to 100");
+        for (; ; ) {
+            try {
+                System.out.println("Enter your guess");
+                int num = scan.nextInt();
+                if (num >= 1 && num <= 100) { // если условие перед ретурном - не будет работать!
+                    return num;
+                } else {
+                    System.out.println("Please enter a number from 1 to 100");
+                }
+            } catch (InputMismatchException e) {
+                String str = scan.next();
+                System.out.println(str + " isn't a number");   // тоесть, пользователю "скажем - то что ты ввел str+ не является чилом!))
             }
         }
     }
